69dc11db960cd51e0455c4c2bc4b948b473ec0ef
Marko Aalto <marko.aalto@reaktor.fi>
2016-03-16 22:53:11 +0200

    Initialize WebGL context

--- gfx.js
+++ gfx.js
@@ -1,6 +1,15 @@
 function gfxRender(canvas, state) {
-  var gc = canvas.getContext("2d")
-  gc.fillStyle = "red"
-  gc.clearRect(0, 0, canvas.width, canvas.height)
-  gc.fillRect(10, 10, state.time*200, state.time*200)
+  var gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl")
+  if (!state.gfxIsInitialized) {
+    initialize()
+    state.gfxIsInitialized = true
+  }
+
+  gl.clearColor(1, 0, 0, 1)
+  gl.clear(gl.COLOR_BUFFER_BIT)
+
+  function initialize() {
+    console.log("*** gfx init", gl)
+  }
 }
+
